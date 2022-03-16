66a8fd8072487a5c8cfe5f1f0369555bb9ca66f7
razvangherman22 <razvan.gherman22@gmail.com>
2022-03-16 18:57:03 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    final static Scanner scanner = new Scanner(System.in);
+    static void greet(String assistantName, String birthYear) {
+        System.out.println("Hello! My name is " + assistantName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+    }
+    static void remindName() {
+        String name = scanner.nextLine();
+        System.out.println("What a great name you have, " + name + "!");
+    }
+    static void guessAge() {
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+    static void count() {
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+        int num = scanner.nextInt();
+        for (int i = 0; i <= num; i++) {
+            System.out.println(i+"!");
+        }
+    }
+    static void test() {
+        String default_answer="4";
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Why do we use methods?");
+        System.out.println("1. To repeat a statement multiple times.\n");
+        System.out.println("2. To decompose a program into several small subroutines.\n");
+        System.out.println("3. To determine the execution time of a program.\n");
+        System.out.println("4. To interrupt the execution of a program.\n");
+        String answer= scanner.next();
+
+        while(answer!=default_answer) {
+            if (answer.equals(default_answer)) {
+                end();
+                return;
+            }
+            System.out.println("Please, try again.\n");
+            answer = scanner.next();
+
+        }
+    }
+
+    static void end() {
+        System.out.println("Congratulations, have a nice day!"); // Do not change this text
+    }
+    public static void main(String[] args) {
+        greet("John","2000");
+        remindName();
+        guessAge();
+        count();
+        test();
+    }
+}
